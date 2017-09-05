9715e622b5600620d9349a94315a7fd71a50ca4c
msharaf <msharaf_m10@yahoo.com>
2017-09-05 03:49:55 +0200

    add login and signup routes

--- routes/routes.js
+++ routes/routes.js
@@ -7,9 +7,37 @@ const config = require( '../models/config' );
 const app = express();
 const router = express.Router();
 
-// routes
+// =====================================
+// Routes
+// =====================================
+
+// Users
 router.get( '/', ( req, res, next ) => {
-    res.sendFile(path.join(__dirname, '../public/views', 'index.html'));
+    res.sendFile( path.join( __dirname, '../public/views', 'index.html' ) );
 });
+
+router.get( '/signup', ( req, res, next ) => {
+    res.sendFile( path.join( __dirname, '../public/views', 'signup.html' ) );
+});
+
+router.post( '/login', ( req, res, next ) => {
+    request.post( config.apiURL + '/auth/token', { form: req.body } ).pipe( res );
+});
+
+router.put( '/users/update', ( req, res, next ) => {
+    request.put( config.apiURL + '/users/' + req.body.id, {
+        headers: { 'x-access-token': req.headers['x-access-token'] },
+        form: req.body
+    }).pipe(res);
+});
+
+router.post( '/signup', ( req, res, next ) => {
+    request.post( config.apiURL + '/users', { form: req.body } ).pipe( res );
+});
+
+// Teams
+
+
+// Rooms
 
 module.exports = router;
