e957173acac9da0308c91393b02b95c22ffb7d32
msharaf <msharaf_m10@yahoo.com>
2017-09-06 11:30:43 +0200

    modify signup jsx and separate signup-form file to 2 components for "user" and "admin"

--- controllers/components/signup/signup-form.jsx
+++ controllers/components/signup/signup-form.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import minified from 'minified';
 
 import regex from '../helpers/regex';
 
-export default class SignupForm extends React.Component {
+export class SignupForAdmin extends React.Component {
     constructor( props ) {
         super( props );
 
@@ -11,7 +10,6 @@ export default class SignupForm extends React.Component {
             formIsValide: '',
             errorMessage: '',
             displayError: '',
-            usernameToken: '',
             submitButton: 'Create account',
             firstName: '',
             lastName: '',
@@ -19,8 +17,245 @@ export default class SignupForm extends React.Component {
             email: '',
             password: '',
             checkPassword: '',
-            companyName: '',
-            isAdmin: ''
+            companyName: ''
+        };
+
+        this._handleChange = this._handleChange.bind( this );
+        this._adminInputIsValid = this._adminInputIsValid.bind( this );
+        this._signupAdmin = this._signupAdmin.bind( this );
+    }
+
+    _handleChange( e ) {
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [ name ]: value
+        });
+
+        if ( [ name ] == 'password' ) {
+            if ( value.length <= 5 ) {
+                this.setState({
+                    checkPassword: 'Week password'
+                });
+            } else if ( value.length < 9 ) {
+                this.setState({
+                    checkPassword: 'semi strong password'
+                });
+            } else {
+                this.setState({
+                    checkPassword: ''
+                });
+            }
+        } // TODO check username length here
+    }
+
+    _checkEmptyFields( target, emptyFields ) {
+        var emptyFeilds = [];
+        for ( var i = 0; i < target.length; i++ ) {
+            var child = target[i];
+            if ( child.nodeName === 'INPUT' ) {
+                if ( child.value == '' && child.hasAttribute( 'required' ) ) {
+                    emptyFeilds.push( child );
+                }
+            }
+        }
+        return emptyFeilds;
+    }
+
+    _adminInputIsValid() {
+        let form = document.forms[ 0 ];
+        let emptyFeilds = this._checkEmptyFields( form );
+        let emptyFeildsNames = [];
+
+        if ( emptyFeilds.length ) {
+            for ( var i in emptyFeilds ) {
+                emptyFeildsNames.push( emptyFeilds[ i ].placeholder );
+            }
+        }
+
+        if (  emptyFeildsNames.length ) {
+            if ( emptyFeildsNames.length <= 3 ) {
+                var keyword = emptyFeildsNames.length === 1 ? 'field!' : 'fields!';
+                this.setState({
+                    errorMessage: 'please fill - ' + emptyFeildsNames.toString() + ' - ' + keyword,
+                    displayError: 'alert alert-danger',
+                    submitButton: 'Create account'
+                });
+                return false;
+            } else {
+                this.setState({
+                    errorMessage: 'please fill in required fields!',
+                    displayError: 'alert alert-danger',
+                    submitButton: 'Create account'
+                });
+                return false;
+            }
+        } else {
+            this.setState({
+                errorMessage: '',
+                displayError: '',
+            });
+        }
+
+        if ( !( regex.email ).test( this.state.email ) ) {
+            this.setState({
+                errorMessage: 'Wrong email',
+                displayError: 'alert alert-danger',
+                submitButton: 'Create account'
+            });
+            return false;
+        }
+
+        if ( this.state.password.length < 5 ) {
+            this.setState({
+                errorMessage: 'Week password',
+                displayError: 'alert alert-danger',
+                submitButton: 'Create account'
+            });
+            return false;
+        }
+
+        // TODO validat phone
+
+        if ( this.state.phone.length > 11 || this.state.phone.length <= 10 ) {
+            this.setState({
+                errorMessage: 'Please insert a valid phone',
+                displayError: 'alert alert-danger',
+                submitButton: 'Create account'
+            });
+            return false;
+        }
+
+        return true;
+    }
+
+    _signupAdmin( e ) {
+        e.preventDefault();
+        const that = this;
+
+        this.setState({
+            formIsValide: 'was-validated',
+            submitButton: 'loading...'
+        });
+
+        if ( !this._adminInputIsValid() ) {
+            return false;
+        }
+
+        let random_avatar = 'ava_' + ( Math.round( Math.random() * 7 ) + 1 );
+
+        var data = {
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            email: this.state.email,
+            phone: this.state.phone,
+            password: this.state.password,
+            companyName: this.state.companyName,
+            isAdmin: true,
+            profPic: random_avatar
+        }
+
+        fetch( '/signup', {
+    	    headers: { 'Content-Type': 'application/json' },
+    	    method: 'POST',
+    	    body: JSON.stringify( data )
+    	}).then(function( res ) {
+    	    if ( !res.ok ) return errorHandler( res );
+    	    res.json().then( signupSuccess );
+    	}).catch( errorHandler );
+
+        function signupSuccess( res ) {
+            alert( 'success' );
+            localStorage.token = res.token;
+            that.setState({
+                errorMessage: 'Registered Successfully',
+                displayError: 'alert alert-success'
+            });
+            setTimeout( function() {
+                window.location = '/';
+            }, 3000 );
+        }
+
+        function errorHandler( err, message ) {
+            if ( err.status >= 400 && err.status < 500 )
+                return err.text().then( function( message ) {
+                    that.setState({
+                        errorMessage: message,
+                        displayError: 'alert alert-danger',
+                        submitButton: 'Create account'
+                    });
+                });
+            if ( message ) return that.setStatus({
+                errorMessagemessage: message,
+                displayError: 'alert alert-danger',
+                submitButton: 'Create account'
+            });
+            return that.setState({
+                errorMessage: 'Sorry there\'s a problem with connection, please try again later',
+                displayError: 'alert alert-danger',
+                submitButton: 'Create account'
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div className = 'container'>
+                <div className = 'title-wrap'>
+                    <h2 className = 'signup-title'>Sign up</h2>
+                    <p>Have account? <a href = '/' className = 'redirect-login'>Login</a></p>
+                </div>
+                <div className = 'signup-form-wrap'>
+                    <a className = 'redirect-form' href = '/signup'><i className = 'fa fa-long-arrow-left' aria-hidden = 'true'></i>Signup as user</a>
+                    <form className = { 'signup-form ' + this.state.formIsValide }>
+                        <div className = { this.state.displayError } role = 'alert'>{ this.state.errorMessage }</div>
+                        <div className = 'form-row'>
+                            <div className = 'form-group col'>
+                                <input className = 'form-control form-control-sm' value = { this.state.firstName } onChange = { this._handleChange } placeholder = 'First name' name = 'firstName' autoFocus required />
+                            </div>
+                            <div className = 'form-group col'>
+                                <input className = 'form-control form-control-sm' value = { this.state.lastName } onChange = { this._handleChange } placeholder = 'Last name' name = 'lastName' required />
+                            </div>
+                        </div>
+                        <div className = 'form-group'>
+                            <input className = 'form-control form-control-sm' value = { this.state.phone } onChange = { this._handleChange } placeholder = 'Phone' name = 'phone' required />
+                        </div>
+                        <div className = 'form-group'>
+                            <input className = 'form-control form-control-sm' value = { this.state.email } onChange = { this._handleChange } type = 'email' placeholder = 'email' name = 'email' required />
+                        </div>
+                        <div className = 'form-group'>
+                            <input className = 'form-control form-control-sm' value = { this.state.password } onChange = { this._handleChange } type = 'password' placeholder = 'password' name = 'password' required />
+                        <div className = 'invalid-feedback'>{ this.state.checkPassword }</div>
+                        </div>
+                        <div className = 'form-group'>
+                            <input className = 'form-control form-control-sm' value = { this.state.companyName } onChange = { this._handleChange } placeholder = 'Company name' name = 'companyName' required />
+                        </div>
+                        <button onClick = { this._signupAdmin } className = 'btn btn-primary'>{ this.state.submitButton }</button>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export class SignupForUser extends React.Component {
+    constructor( props ) {
+        super( props );
+
+        this.state = {
+            formIsValide: '',
+            errorMessage: '',
+            displayError: '',
+            usernameToken: '', // TODO check if username is token or length < 5 chars
+            submitButton: 'Create account',
+            firstName: '',
+            lastName: '',
+            phone: '',
+            email: '',
+            password: '',
+            checkPassword: ''
         };
 
         this._handleChange = this._handleChange.bind( this );
@@ -125,8 +360,8 @@ export default class SignupForm extends React.Component {
 
         if ( this.state.phone.length > 11 || this.state.phone.length <= 10 ) {
             this.setState({
-                errorMessage: 'Wrong phone',
-                displayError: 'Please insert a valid phone',
+                errorMessage: 'Please insert a valid phone',
+                displayError: 'alert alert-danger',
                 submitButton: 'Create account'
             });
             return false;
@@ -148,6 +383,8 @@ export default class SignupForm extends React.Component {
             return false;
         }
 
+        let random_avatar = 'ava_' + ( Math.round( Math.random() * 7 ) + 1 );
+
         var data = {
             firstName: this.state.firstName,
             lastName: this.state.lastName,
@@ -156,7 +393,8 @@ export default class SignupForm extends React.Component {
             phone: this.state.phone,
             password: this.state.password,
             companyName: this.state.companyName,
-            isAdmin: this.state.isAdmin
+            isAdmin: this.state.isAdmin,
+            profPic: random_avatar
         }
 
         fetch( '/signup', {
@@ -203,6 +441,7 @@ export default class SignupForm extends React.Component {
     }
 
     render() {
+        const usernameNote = 'NOTE: Choose username carefully which will be your name to other teammates and get used for searching';
         return (
             <div className = 'container'>
                 <div className = 'title-wrap'>
@@ -210,6 +449,7 @@ export default class SignupForm extends React.Component {
                     <p>Have account? <a href = '/' className = 'redirect-login'>Login</a></p>
                 </div>
                 <div className = 'signup-form-wrap'>
+                    <a className = 'redirect-form' href = '/signup/admin'>Signup as admin<i className = 'fa fa-long-arrow-right' aria-hidden = 'true'></i></a>
                     <form className = { 'signup-form ' + this.state.formIsValide }>
                         <div className = { this.state.displayError } role = 'alert'>{ this.state.errorMessage }</div>
                         <div className = 'form-row'>
@@ -224,7 +464,7 @@ export default class SignupForm extends React.Component {
                             <input className = 'form-control form-control-sm' value = { this.state.phone } onChange = { this._handleChange } placeholder = 'Phone' name = 'phone' required />
                         </div>
                         <div className = 'form-group'>
-                            <input className = 'form-control form-control-sm' value = { this.state.username } onChange = { this._handleChange } placeholder = 'Username' name = 'username' required />
+                            <input className = 'form-control form-control-sm username-tooltip' value = { this.state.username } onChange = { this._handleChange } placeholder = 'Username' name = 'username' data-toggle = 'tooltip' data-placement = 'left' title = { usernameNote } required />
                             <div className = 'invalid-feedback'>{ this.state.usernameToken }</div>
                         </div>
                         <div className = 'form-group'>
@@ -233,9 +473,6 @@ export default class SignupForm extends React.Component {
                         <div className = 'form-group'>
                             <input className = 'form-control form-control-sm' value = { this.state.password } onChange = { this._handleChange } type = 'password' placeholder = 'password' name = 'password' required />
                         <div className = 'invalid-feedback'>{ this.state.checkPassword }</div>
-                        </div>
-                        <div className = 'form-group'>
-                            <input className = 'form-control form-control-sm' value = { this.state.companyName } onChange = { this._handleChange } placeholder = 'Company name' name = 'companyName' required />
                         </div>
                         <button onClick = { this._signupUser } className = 'btn btn-primary'>{ this.state.submitButton }</button>
                     </form>
--- controllers/components/signup/signup.jsx
+++ controllers/components/signup/signup.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import SignupForm from './signup-form.jsx';
+import Navbar from '../navbar.jsx';
+import { SignupForAdmin, SignupForUser } from './signup-form.jsx';
 
 class Main extends React.Component {
-    tokenIsValid ( token ) {
-        if ( !token ){
-            return false;
-        } else if ( token ) {
-            const userToken = JSON.parse( atob( token.split( '.' )[ 1 ] ) );
-            if ( !userToken.hasOwnProperty( 'id' ) )
-                return false;
-            else if ( !userToken )
-                return false;
-            return true;
+    constructor() {
+        super();
+
+        this.state = {
+            isAdmin: false
+        };
+    }
+
+    componentWillMount() {
+        if ( window.location.href.indexOf( 'admin' ) > -1 ) {
+            this.setState({
+                isAdmin: true
+            });
         }
     }
+
     render() {
-        const content = this.tokenIsValid( localStorage.token ) ? <h1>Not allowd</h1> : <SignupForm />;
+        const Form = this.state.isAdmin ? <SignupForAdmin /> : <SignupForUser />;
         return (
             <div className='content-wrap'>
                 <div className='content'>
-                    { content }
+                    { Form }
                 </div>
             </div>
         );
@@ -29,7 +34,4 @@ class Main extends React.Component {
 }
 
 const root = document.getElementById( 'root' );
-ReactDOM.render(
-    <Main />,
-    root
-);
+ReactDOM.render( <Main />, root );
