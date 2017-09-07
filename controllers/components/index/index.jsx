69f84893f1f3b0feef1b29779e4156481fed2b40
msharaf <msharaf_m10@yahoo.com>
2017-09-07 13:44:11 +0200

    modify index.jsx and add one regex helper

--- controllers/components/index/index.jsx
+++ controllers/components/index/index.jsx
@@ -2,22 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import LoginForm from './login-form.jsx';
+import Navbar from '../navbar.jsx';
 
 class Main extends React.Component {
+
     tokenIsValid ( token ) {
         if ( !token ){
             return false;
         } else if ( token ) {
-            const userToken = JSON.parse( atob( token.split( '.' )[ 1 ] ) );
-            if ( !userToken.hasOwnProperty( 'id' ) )
+            const user = JSON.parse( atob( localStorage.token.split( '.' )[ 1 ] ) );
+            if ( !user.hasOwnProperty( 'id' ) )
                 return false;
-            else if ( !userToken )
+            else if ( !user )
                 return false;
             return true;
         }
     }
+
     render() {
-        const content = this.tokenIsValid( localStorage.token ) ? <h1>Not allowd</h1> : <LoginForm />;
+        const content = this.tokenIsValid( localStorage.token ) ? <Navbar /> : <LoginForm />;
         return (
             <div className='content-wrap'>
                 <div className='content'>
