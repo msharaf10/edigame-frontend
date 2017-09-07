3699fa2dcc6aa11581a4e1a2c79a01d2f519bd2c
msharaf <msharaf_m10@yahoo.com>
2017-09-07 13:39:30 +0200

    add error handler middleware

--- index.js
+++ index.js
@@ -15,7 +15,34 @@ app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded({ extended: true }) );
 app.use( express.static( path.join(__dirname, 'public' )));
 
+// =====================================
+// Routes
+// =====================================
 app.use( '/', routes );
+
+// =====================================
+// Error Handler
+// =====================================
+app.use( ( req, res, next ) => {
+	let err = new Error( 'NOT FOUND' );
+	err.status =  404;
+	next( err );
+});
+
+app.use( ( err, req, res, next ) => {
+	if ( app.get( 'env' ) === 'development' ) console.log( err.message );
+
+	let status = err.status || 500;
+	let message;
+
+	if ( status >= 400 && status < 500 && err.message ) {
+		message = err.message;
+	} else {
+		message = '';
+
+	}
+	res.status( status ).send( message );
+});
 
 let server = app.listen( config.port ),
 	port = server.address().port,
