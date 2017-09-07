0d4a37df5f91e3993d7557c51ec5e67a2d355421
msharaf <msharaf_m10@yahoo.com>
2017-09-07 13:35:13 +0200

    add fetch for user's team

--- controllers/components/navbar.jsx
+++ controllers/components/navbar.jsx
@@ -4,18 +4,40 @@ export default class Admin_Navbar extends React.Component {
     constructor( props ){
         super( props );
         this.state = {
-            team: ''
+            team: '',
+            teamLink: ''
         }
         this._handleSearch = this._handleSearch.bind( this );
     }
 
     componentDidMount() {
+        const that = this;
         const user = JSON.parse( atob( localStorage.token.split( '.' )[ 1 ] ) );
-        const team = user.isAdmin ? '/admin/team' : '/my-team'
 
-        this.setState({
-            team: team
-        });
+        // TODO make this fetch function update all navbar details
+        fetch( '/users/get-one-user/' + user.id )
+        .then( function( res ) {
+            if ( !res.ok ) return alert( 'failed fetching user' );
+            res.json().then( function( user ) {
+                that.setState({
+                    team: user.team
+                });
+                if ( that.state.team ) { fetchTeamName( user.team ); }
+            });
+        }).catch(function( err ) { alert( err ) } );
+
+        // Fetch team name of user
+        function fetchTeamName( id ) {
+            fetch( '/teams/get-one-team/' + id )
+            .then( function( res ) {
+                if ( !res.ok ) return alert( 'failed fetching team' );
+                res.json().then( function( team ) {
+                    that.setState({
+                        teamLink: team.teamName
+                    });
+                });
+            }).catch( function( err ) { alert( err ) } );
+        }
     }
 
     _logoutUser( e ) {
@@ -26,7 +48,7 @@ export default class Admin_Navbar extends React.Component {
 
     _handleSearch( e ) {
         const form = document.forms[ 's' ];
-        const search_for = prompt( 'Search for?\ntype: \'user\' or \'team\'');
+        const search_for = prompt( 'Search for?\ntype: \'user\' or \'team\'' );
         const value = form.q.value;
 
         if ( !search_for ) {
@@ -50,15 +72,20 @@ export default class Admin_Navbar extends React.Component {
 
     render() {
         const user = JSON.parse( atob( localStorage.token.split( '.' )[ 1 ] ) );
-        let style = { position: 'absolute', left: 'unset' }; // style dropdown menu
+        const userTeam_link = this.state.teamLink ? <a className = 'dropdown-item' href = { '/team/' + this.state.teamLink }>My Team</a> : '';
+
+        const style = {
+            dropdown: { position: 'absolute', left: 'unset' },
+            profilePosition: { backgroundPosition: user.profPic.x + 'px '  + user.profPic.y + 'px' }
+        };
 
         return (
             <div className = 'container-fluid navbar-wrap'>
                 <div className = 'container'>
                     <nav className = 'navbar justify-content-between'>
                         <div className = 'navbar-brand'>
-                            <img src = { 'img/profile/' + user.profPic + '.png' } width = '30' height = '30' className = 'user-pic d-inline-block align-top' alt = { user.name } />
-                            <span className = 'user-name'>{ user.name }</span>
+                            <span style = { style.profilePosition }className = 'user-pic d-inline-block align-top'></span>
+                        <span className = 'user-name'>{ user.name }</span>
                         </div>
                         <div className = 'my-2 my-lg-0'>
                             <form className = 'form-inline my-2 my-lg-0' action = '/search' method = 'GET' role = 'search' onSubmit = { this._handleSearch } name = 's'>
@@ -70,8 +97,9 @@ export default class Admin_Navbar extends React.Component {
                                 <li className = 'nav-item dropdown'>
                                     <a href = '#' className = 'nav-link dropdown-toggle' id = 'navbarDropdownMenuLink' data-toggle = 'dropdown' aria-haspopup = 'true' aria-expanded = 'false'>
                                     </a>
-                                    <div className = 'dropdown-menu' aria-labelledby = 'navbarDropdownMenuLink' style = { style }>
-                                        <a className = 'dropdown-item' href = { this.state.team }>My Team</a>
+                                    <div className = 'dropdown-menu' aria-labelledby = 'navbarDropdownMenuLink' style = { style.dropdown }>
+                                        { userTeam_link }
+                                        <a className = 'dropdown-item' href = '/players'>All Players</a>
                                         <a className = 'dropdown-item' href = '/settings'>Settings</a>
                                         <a className = 'dropdown-item' href = '#' onClick = { this._logoutUser }>Logout</a>
                                     </div>
