7e1f7d84294d5886fa410f3b5ceea0180173f401
msharaf <msharaf_m10@yahoo.com>
2017-09-07 13:26:24 +0200

    remove alerts from addPlayer and use errorHandler function instead

--- controllers/components/users/users.jsx
+++ controllers/components/users/users.jsx
@@ -10,9 +10,11 @@ class FetchUsers extends React.Component {
 
         this.state = {
             users: [],
-            message: ''
+            message: '',
+            displayError: '',
+            errorMessage: ''
         };
-        this._addPlayer = this._addPlayer.bind( this );
+        this._handleClick = this._handleClick.bind( this );
     }
 
     componentDidMount() {
@@ -22,6 +24,7 @@ class FetchUsers extends React.Component {
     _fetchUsers() {
         const that = this;
 
+        // TODO clean fetch and define global errorHandler function
         fetch( '/users/get-all-users')
         .then( function( res ) {
             if ( !res.ok ) return alert( 'failed to fetch users' );
@@ -29,13 +32,68 @@ class FetchUsers extends React.Component {
         }).catch( ( err ) => { alert ( err ) } );
     }
 
-    _addPlayer( e ) {
+    _handleClick( e ) {
         e.preventDefault();
-        const user = e.target.getAttribute( 'data-id' );
+
+        const playerId = e.target.getAttribute( 'data-id' );
         const admin = JSON.parse( atob( localStorage.token.split( '.' )[ 1 ] ) );
+
+        this.setState({
+            errorMessage: '',
+            displayError: ''
+        });
 
         if ( !admin.team ) {
             alert( 'you cant add players create-new-team first' );
+        } else {
+
+            this._addPlayer( playerId, admin.team )
+        }
+    }
+
+    _addPlayer( playerId, team ) {
+        const that = this;
+
+        let data = {
+            playerId: playerId,
+            teamId: team
+        };
+
+        fetch( '/teams/add-player', {
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': localStorage.token
+            },
+            method: 'POST',
+            body: JSON.stringify( data )
+        }).then( function( res ) {
+            if ( !res.ok ) return errorHandler( res );
+            res.json().then (addPlayerSuccess);
+        }).catch( errorHandler );
+
+        function addPlayerSuccess() {
+            return that.setStatus({
+                errorMessage: 'Player added :)',
+                displayError: 'alert alert-success'
+            });
+        }
+
+        function errorHandler( err, message ) {
+            if ( err.status >= 400 && err.status < 500 )
+                return err.text().then( function( message ) {
+                    that.setState({
+                        errorMessage: message,
+                        displayError: 'alert alert-danger'
+                    });
+                });
+            if ( message ) return that.setStatus({
+                errorMessage: message,
+                displayError: 'alert alert-danger'
+            });
+            return that.setState({
+                errorMessage: 'Sorry there\'s a problem with connection, please try again later',
+                displayError: 'alert alert-danger'
+            });
         }
     }
 
@@ -44,7 +102,7 @@ class FetchUsers extends React.Component {
             const user = JSON.parse( atob( localStorage.token.split( '.' )[ 1 ] ) );
             if ( user.isAdmin ) {
                 if ( user.id !== id ) {
-                    return <button className = 'btn btn-info btn-sm' onClick = { this._addPlayer } data-id = { id }>Show Name</button>;
+                    return <button className = 'btn btn-info btn-sm' onClick = { this._handleClick } data-id = { id }>Show Name</button>;
                 } else {
                     return '';
                 }
@@ -57,6 +115,8 @@ class FetchUsers extends React.Component {
     }
 
     render() {
+        // TODO put the following in a new component or method in current component
+        // TODO pass this.state.user to new array then use filter method to remove current user
         const users = this.state.users.map( ( user, i ) => {
             const style = { backgroundPosition: user.profPic.x + 'px '  + user.profPic.y + 'px' }
             return (
@@ -65,7 +125,7 @@ class FetchUsers extends React.Component {
                         <ul className = 'user-list-wrap' key = { i }>
                             <li className = 'user-item user-name-wrap'>
                                 <span style = { style }className = 'user-pic d-inline-block align-top'></span>
-                                <span ref = {( _user ) => this._username = _user}>{ user.firstName + ' ' + user.lastName }</span>
+                                <span>{ user.firstName + ' ' + user.lastName }</span>
                             </li>
                             <li className = 'user-item'>email: { user.email }</li>
                             <li className = 'user-item'>phone: { user.phone }</li>
@@ -80,6 +140,7 @@ class FetchUsers extends React.Component {
             <div>
                 <div className = 'container'>
                     <h1>Fetch for players</h1>
+                    <div className = { this.state.displayError } role = 'alert'>{ this.state.errorMessage }</div>
                     <div className = 'row'>{ users }</div>
                 </div>
             </div>
