83b5c9a871cb07e5d4ee0609cfcfa800d07b4b71
msharaf <msharaf_m10@yahoo.com>
2017-09-08 07:49:23 +0200

    add remove player functionality

--- controllers/components/users/users.jsx
+++ controllers/components/users/users.jsx
@@ -14,7 +14,8 @@ class FetchUsers extends React.Component {
             displayError: '',
             errorMessage: ''
         };
-        this._handleClick = this._handleClick.bind( this );
+        this._addPlayer = this._addPlayer.bind( this );
+        this._removePlayer = this._removePlayer.bind( this );
     }
 
     componentDidMount() {
@@ -27,12 +28,12 @@ class FetchUsers extends React.Component {
         // TODO clean fetch and define global errorHandler function
         fetch( '/users/get-all-users')
         .then( function( res ) {
-            if ( !res.ok ) return alert( 'failed to fetch users' );
+            if ( !res.ok ) return that._errorHandler( err );
             res.json().then( ( users ) => { that.setState({ users }) });
         }).catch( ( err ) => { alert ( err ) } );
     }
 
-    _handleClick( e ) {
+    _addPlayer( e ) {
         e.preventDefault();
 
         const playerId = e.target.getAttribute( 'data-id' );
@@ -43,15 +44,24 @@ class FetchUsers extends React.Component {
             displayError: ''
         });
 
-        if ( !admin.team ) {
-            alert( 'you cant add players create-new-team first' );
-        } else {
-
-            this._addPlayer( playerId, admin.team )
-        }
+        this._pushPlayerToTeam( playerId, admin.team )
     }
 
-    _addPlayer( playerId, team ) {
+    _removePlayer( e ) {
+        e.preventDefault();
+
+        const playerId = e.target.getAttribute( 'data-id' );
+        const admin = JSON.parse( atob( localStorage.token.split( '.' )[ 1 ] ) );
+
+        this.setState({
+            errorMessage: '',
+            displayError: ''
+        });
+
+        this._removePlayerFromTeam( playerId, admin.team )
+    }
+
+    _removePlayerFromTeam( playerId, team ) {
         const that = this;
 
         let data = {
@@ -59,50 +69,86 @@ class FetchUsers extends React.Component {
             teamId: team
         };
 
-        fetch( '/teams/add-player', {
+        fetch( '/api/teams/remove/player', {
             headers: {
                 'Content-Type': 'application/json',
                 'x-access-token': localStorage.token
             },
             method: 'POST',
             body: JSON.stringify( data )
-        }).then( function( res ) {
-            if ( !res.ok ) return errorHandler( res );
-            res.json().then (addPlayerSuccess);
-        }).catch( errorHandler );
+        }).then( removePlayerSuccess )
+        .catch( that._errorHandler );
 
-        function addPlayerSuccess() {
-            return that.setStatus({
-                errorMessage: 'Player added :)',
+        function removePlayerSuccess( res ) {
+            if ( !res.ok ) return that._errorHandler( res );
+
+            that.setState({
+                errorMessage: 'Player removed :(',
                 displayError: 'alert alert-success'
-            });
-        }
-
-        function errorHandler( err, message ) {
-            if ( err.status >= 400 && err.status < 500 )
-                return err.text().then( function( message ) {
-                    that.setState({
-                        errorMessage: message,
-                        displayError: 'alert alert-danger'
-                    });
-                });
-            if ( message ) return that.setStatus({
-                errorMessage: message,
-                displayError: 'alert alert-danger'
-            });
-            return that.setState({
-                errorMessage: 'Sorry there\'s a problem with connection, please try again later',
-                displayError: 'alert alert-danger'
             });
         }
     }
 
-    _addPlayerButton( id ) {
+    _pushPlayerToTeam( playerId, team ) {
+        const that = this;
+
+        let data = {
+            playerId: playerId,
+            teamId: team
+        };
+
+        fetch( '/api/teams/add/player', {
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': localStorage.token
+            },
+            method: 'POST',
+            body: JSON.stringify( data )
+        }).then( addPlayerSuccess )
+        .catch( that._errorHandler );
+
+        function addPlayerSuccess( res ) {
+            if ( !res.ok ) return that._errorHandler( res );
+
+            that.setState({
+                errorMessage: 'Player added :)',
+                displayError: 'alert alert-success'
+            });
+        }
+    }
+
+    _errorHandler( err, message ) {
+        const that = this;
+        if ( err.status >= 400 && err.status < 500 )
+            return err.text().then( function( message ) {
+                that.setState({
+                    errorMessage: message,
+                    displayError: 'alert alert-danger'
+                });
+            });
+        if ( message ) return that.setStatus({
+            errorMessage: message,
+            displayError: 'alert alert-danger'
+        });
+        alert( message );
+        return that.setState({
+            errorMessage: 'Sorry there\'s a problem with connection, please try again later',
+            displayError: 'alert alert-danger'
+        });
+    }
+
+    _addPlayerButton( user ) {
         if ( localStorage.token ) {
-            const user = JSON.parse( atob( localStorage.token.split( '.' )[ 1 ] ) );
-            if ( user.isAdmin ) {
-                if ( user.id !== id ) {
-                    return <button className = 'btn btn-info btn-sm' onClick = { this._handleClick } data-id = { id }>Show Name</button>;
+            const admin = JSON.parse( atob( localStorage.token.split( '.' )[ 1 ] ) );
+            if ( admin.isAdmin ) {
+                if ( admin.team ) {
+                    if ( !user.team ) {
+                        return <button className = 'btn btn-info btn-xs' onClick = { this._addPlayer } data-id = { user._id }>Add Player</button>;
+                    } else if ( user.team == admin.team ) {
+                        return <button className = 'btn btn-danger btn-xs' onClick = { this._removePlayer } data-id = { user._id }>Remove Player</button>;
+                    } else {
+                        return '';
+                    }
                 } else {
                     return '';
                 }
@@ -114,34 +160,40 @@ class FetchUsers extends React.Component {
         }
     }
 
-    render() {
-        // TODO put the following in a new component or method in current component
-        // TODO pass this.state.user to new array then use filter method to remove current user
+    _showUsers() {
         const users = this.state.users.map( ( user, i ) => {
             const style = { backgroundPosition: user.profPic.x + 'px '  + user.profPic.y + 'px' }
-            return (
-                <div className = 'col-6 col-sm-3 user-wrap'>
-                    <div className = 'user-details-wrap'>
-                        <ul className = 'user-list-wrap' key = { i }>
-                            <li className = 'user-item user-name-wrap'>
-                                <span style = { style }className = 'user-pic d-inline-block align-top'></span>
-                                <span>{ user.firstName + ' ' + user.lastName }</span>
-                            </li>
-                            <li className = 'user-item'>email: { user.email }</li>
-                            <li className = 'user-item'>phone: { user.phone }</li>
-                            { this._addPlayerButton( user._id ) }
-                        </ul>
-                    </div>
-                </div>
-            );
-        });
 
+            if ( !user.isAdmin ) {
+                return (
+                    <div className = 'col-6 col-sm-3 user-wrap'>
+                        <div className = 'user-details-wrap'>
+                            <ul className = 'user-list-wrap' key = { i }>
+                                <li className = 'user-item user-name-wrap'>
+                                    <span style = { style }className = 'user-pic d-inline-block align-top'></span>
+                                    <span>{ user.firstName + ' ' + user.lastName }</span>
+                                </li>
+                                <li className = 'user-item'>email: { user.email }</li>
+                                <li className = 'user-item'>phone: { user.phone }</li>
+                                { this._addPlayerButton( user ) }
+                            </ul>
+                        </div>
+                    </div>
+                );
+            } else {
+                return '';
+            }
+        });
+        return users;
+    }
+
+    render() {
         return (
             <div>
                 <div className = 'container'>
                     <h1>Fetch for players</h1>
                     <div className = { this.state.displayError } role = 'alert'>{ this.state.errorMessage }</div>
-                    <div className = 'row'>{ users }</div>
+                    <div className = 'row'>{ this._showUsers() }</div>
                 </div>
             </div>
         );
