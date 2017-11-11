166689e12dc4d4603c9a31b72c438f76d87bfe3a
msharaf10 <msharaf_m10@yahoo.com>
2017-11-11 21:14:21 +0200

    update the structure of src directory

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom'
-
-import App from './containers/App.js';
 
 render(
     <Router>
-        <App />
     </Router>,
     document.getElementById( 'root' )
 );
