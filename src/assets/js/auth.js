bdb5c9c8768175e1fdc695adf8fc3a1ac739dd58
msharaf10 <msharaf_m10@yahoo.com>
2017-11-27 21:09:16 +0200

    add "auth" controller

--- src/assets/js/auth.js
+++ src/assets/js/auth.js
@@ -1,4 +1,24 @@
-(function() {
-    'use strict';
-    
-}());
+import { loadToken } from '../../helpers/auth'
+
+const { token } = loadToken()
+const path = window.location.pathname
+
+if ( !token && path.indexOf( 'teams' ) > -1 )
+    window.location = '/'
+
+switch ( path ) {
+    case '/teams':
+    case '/logout':
+    case '/my-teams':
+    case '/settings':
+    case '/teams/create':
+        if ( token === undefined )
+            window.location = '/'
+        break
+    case '/login':
+    case '/signup':
+    case '/logout':
+        if ( token )
+            window.location = '/'
+        break
+}
