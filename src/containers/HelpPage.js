43971439b4b5a208a616afe61ae1ead66f48898b
msharaf10 <msharaf_m10@yahoo.com>
2017-11-27 17:45:49 +0200

    add samples containers

---
+++ src/containers/HelpPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const HelpPage = () => (
+    <div className = 'container'>
+        <h3>Help Center</h3>
+        { /* help content goes here  */ }
+    </div>
+)
+
+export default HelpPage
